6107abe9f8e7502b4cd1bc58a5b3118f383ec9da
Ahmed <ahmedgamal1994@outlook.com>
2021-12-21 16:35:23 +0200

    [fix] profile issues

--- resources/js/src/chat.js
+++ resources/js/src/chat.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://www.magaxat.com",
+    baseURL: "http://www.magaxat.com/api/",
     headers: {
         "Access-Control-Allow-Origin": "*",
     },
