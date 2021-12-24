89049ca671771c8c415c5cdd47a3920d9cd01cc4
Ahmed <ahmedgamal1994@outlook.com>
2021-12-24 17:33:43 +0200

    [fix] bugs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,4 +38,5 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: false,
+    enabledTransports: ["ws", "wss"],
 });
