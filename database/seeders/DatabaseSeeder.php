b957a2b34e1616dbf877a7e3629a4070f1ead671
Ahmed <ahmedgamal1994@outlook.com>
2022-01-24 09:46:17 +0200

    [fix] responsive issues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
         $this->call(InterestsSeeder::class);
+        $this->call(AdminSeeder::class);
         $this->call(CountriesSeeder::class);
     }
 }
