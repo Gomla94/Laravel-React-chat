09723526f36a5c79b78750a073d19b43af1cd5e9
Ahmed <ahmedgamal1994@outlook.com>
2022-01-26 18:20:35 +0200

    [add] updates

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,8 @@ try {
 window.axios = require("axios");
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-window.axios.defaults.headers.common["X-CSRF-TOKEN"] = window.Laravel.csrfToken;
+window.axios.defaults.headers.common["X-CSRF-TOKEN"] =
+    document.head.querySelector('meta[name="csrf-token"]');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
