4a0ace674b05ccafac1b49f1cc35ceedffddd603
Ahmed <ahmedgamal1994@outlook.com>
2022-01-26 18:14:26 +0200

    [add] updates

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,6 @@ window.axios = require("axios");
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.axios.defaults.headers.common["X-CSRF-TOKEN"] = window.Laravel.csrfToken;
 
-console.log("csrf", window.Laravel.csrfToken);
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/js/src/chat.js
+++ resources/js/src/chat.js
@@ -5,6 +5,7 @@ export default axios.create({
     baseURL: "https://www.magaxat.com/api/",
     headers: {
         "Access-Control-Allow-Origin": "*",
+        "X-CSRF-TOKEN": window.Laravel.csrfToken,
         authorization: `Bearer ${
             window.Laravel ? window.Laravel.user.api_token : null
         }`,
