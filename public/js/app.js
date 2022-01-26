4cd04c1960f92746e6e4e8ac6a5e67abfb502a86
Ahmed <ahmedgamal1994@outlook.com>
2022-01-26 16:49:23 +0200

    [add] new updates

--- public/js/app.js
+++ public/js/app.js
@@ -2137,8 +2137,8 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: "pusher",
   key: "f3410ab18dff50208018",
   cluster: "mt1",
-  encrypted: true // forceTLS: false,
-  // disableStats: true,
+  encrypted: true,
+  forceTLS: true // disableStats: true,
   // wsPort: 6001,
   // wssPort: 6001,
   // enabledTransports: ["ws", "wss"],
@@ -3297,8 +3297,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (axios__WEBPACK_IMPORTED_MODULE_0___default().create({
-  baseURL: "https://seriousapp.test/api/",
-  // baseURL: "http://www.magaxat.com/api/",
+  // baseURL: "https://seriousapp.test/api/",
+  baseURL: "https://www.magaxat.com/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     authorization: "Bearer ".concat(window.Laravel ? window.Laravel.user.api_token : null)
@@ -3322,8 +3322,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (axios__WEBPACK_IMPORTED_MODULE_0___default().create({
-  baseURL: "https://seriousapp.test/api/",
-  // baseURL: "http://www.magaxat.com/api/",
+  // baseURL: "https://seriousapp.test/api/",
+  baseURL: "https://www.magaxat.com/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     authorization: "Bearer ".concat(window.Laravel ? window.Laravel.user.api_token : null)
