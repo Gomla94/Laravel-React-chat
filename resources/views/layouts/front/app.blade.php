47d02d3c21f14e655a0a3e1693a8314c2c911dc5
Ahmed <ahmedgamal1994@outlook.com>
2022-02-18 13:30:04 +0200

    [fix] frontend issues

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> --}}
+    <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no;user-scalable=0;"/>
+
     @yield('meta-description')
     {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
