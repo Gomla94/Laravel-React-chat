4cbbab0e52c04034abc98aa6bd1555486a953a32
Ahmed <ahmedgamal1994@outlook.com>
2022-03-17 17:19:27 +0200

    [add] newest design

--- resources/views/layouts/front/chat.blade.php
+++ resources/views/layouts/front/chat.blade.php
@@ -2,7 +2,7 @@
 @section('meta-description')
 @endsection
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/chat.css?version=10') }}"/>
+<link rel="stylesheet" href="{{ asset('css/chat.css?version=11') }}"/>
 @endsection
 @section('content')
 <div id="root"></div>
