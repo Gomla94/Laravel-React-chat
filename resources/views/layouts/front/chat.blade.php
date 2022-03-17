ba6d4ee678f10f86199acfaa246336c380a5658e
Ahmed <ahmedgamal1994@outlook.com>
2022-03-17 19:03:42 +0200

    [test] new chat

--- resources/views/layouts/front/chat.blade.php
+++ resources/views/layouts/front/chat.blade.php
@@ -5,7 +5,7 @@
 Magaxat | Chat
 @endsection
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/chat.css?version=14') }}"/>
+<link rel="stylesheet" href="{{ asset('css/chat.css?version=15') }}"/>
 @endsection
 @section('content')
 <div id="root"></div>
