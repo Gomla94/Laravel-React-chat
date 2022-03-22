66bea0aee29e224d386cfe31f2c2f46ac5c463a1
Ahmed <ahmedgamal1994@outlook.com>
2022-03-22 20:37:18 +0200

    [add] new translations

--- resources/views/layouts/front/welcome-app.blade.php
+++ resources/views/layouts/front/welcome-app.blade.php
@@ -25,7 +25,7 @@
     />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=30')}}" />
+    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=31')}}" />
     @yield('styles')
     <script src="{{asset('js/newest-index.js?version=3')}}" defer></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
