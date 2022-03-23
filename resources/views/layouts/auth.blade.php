aea0f75a0cc95194e3886bf9529cc4fe349ef37e
Ahmed <ahmedgamal1994@outlook.com>
2022-03-23 16:27:01 +0200

    [change] profile image

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,7 +17,7 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
     />
-    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=46')}}" />
+    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=47')}}" />
     <script src="{{asset('js/newest-index.js?version=2')}}" defer></script>
     <title>@yield('title')</title>
   </head>
