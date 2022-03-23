2b148db5c30c4b3cd3e7a98d0cdf4e5ba5511c49
Ahmed <ahmedgamal1994@outlook.com>
2022-03-23 08:13:23 +0200

    [fix] design issues

--- resources/views/layouts/front/chat.blade.php
+++ resources/views/layouts/front/chat.blade.php
@@ -5,7 +5,7 @@
 Magaxat | Chat
 @endsection
 @section('styles')
-<link rel="stylesheet" href="{{ asset('css/chat.css?version=25') }}"/>
+<link rel="stylesheet" href="{{ asset('css/chat.css?version=26') }}"/>
 @endsection
 @section('content')
 <div id="root"></div>
