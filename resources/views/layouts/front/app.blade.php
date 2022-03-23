80f0b3146e47b26d31839c92c013cff7cab48dd6
Ahmed <ahmedgamal1994@outlook.com>
2022-03-23 15:04:46 +0200

    [fix] design issues

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -25,7 +25,7 @@
     />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=50')}}" />
+    <link rel="stylesheet" href="{{asset('css/newest-index.css?version=51')}}" />
 
     @yield('styles')
     <script src="{{asset('js/newest-index.js?version=4')}}" defer></script>
