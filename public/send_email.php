eff5c30d08c7bedb90f736e98b7caaa2aa8f4b52
arsen00arsen <arsenbabayan92@gmail.com>
2022-03-29 20:40:47 +0400

    adding favicon

--- public/send_email.php
+++ public/send_email.php
@@ -4,12 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "magaghat-one-page";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
