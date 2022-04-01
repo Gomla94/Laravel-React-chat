fabc9006e6ad828dcc105e9f2139250a5f55aacf
Ahmed <ahmedgamal1994@outlook.com>
2022-04-01 11:31:50 +0200

    [fix] some issues

--- resources/views/layouts/front/welcome.blade.php
+++ resources/views/layouts/front/welcome.blade.php
@@ -365,7 +365,7 @@
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
 <script src="{{ asset('js/addPostLike.js') }}" defer type="module"></script>
-<script src="{{ asset('js/toggleModalInputs.js?version=2') }}" defer></script>
+<script src="{{ asset('js/toggleModalInputs.js?version=3') }}" defer></script>
 <script src="{{ asset('js/loadPosts.js?version=7') }}" defer type="module"></script>
 <script src="{{asset('js/newest-addPostComments.js')}}" defer type="module"></script>
 <script src="{{asset('js/upload.js')}}" defer></script>
